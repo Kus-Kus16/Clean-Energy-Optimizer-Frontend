367ba71c88c1f0edcac041e32882841522397cbb
KusKus <maciekkus16@gmail.com>
2026-01-17 16:16:15 +0100

    added preview allowed hosts

--- src/api/Axios.tsx
+++ src/api/Axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = import.meta.env.VITE_BACKEND_URL;
+export const baseURL = import.meta.env.VITE_BACKEND_URL ?? "https://clean-energy-optimizer-backend.onrender.com" ;
 
 const api = axios.create({
     baseURL: baseURL,
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const FRONTEND_HOST = process.env.VITE_FRONTEND_HOST || 'localhost';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
+    host: '0.0.0.0',
+    port:5173
+  },
+  preview: {
       host: '0.0.0.0',
-      port:5173
-    }
+      port: 4173,
+      allowedHosts: [FRONTEND_HOST],
+  },
 })
